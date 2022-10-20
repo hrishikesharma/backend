08e13999a5e2535909790b3892ad2bfcba50ccf2
Your Name~ <you@example.com>
2022-10-20 17:23:33 +0530

    initial commit

--- index.js
+++ index.js
@@ -29,51 +29,60 @@
 // console.log(NAME);
 
 
-const parser = require('simple-excel-to-json')
-const doc = parser.parseXls2Json('./Example.xlsx')[0]; 
-const json2xls = require("json2xls")
-const fs = require("fs");
+// const parser = require('simple-excel-to-json')
+// const doc = parser.parseXls2Json('./Example.xlsx')[0]; 
+// const json2xls = require("json2xls")
+// const fs = require("fs");
+
 
 //console.log(doc)
 
-const totalCgpa = doc.reduce((prevValue, currentValue) => {
-    prevValue += currentValue.CGPA
-    return prevValue
-}, 0)
+// const totalCgpa = doc.reduce((prevValue, currentValue) => {
+//     prevValue += currentValue.CGPA
+//     return prevValue
+// }, 0)
 
-const averageCGPA = totalCgpa / doc.length;
+// const averageCGPA = totalCgpa / doc.length;
 
 // console.log(totalCgpa)
 // console.log(averageCGPA)
 
 // A+ = 9.5, A=9 and above, B=8.5 and above, C=8
 
-const gradedDocument = doc.map((student) => {
-    if(student.CGPA >= 9.5) {
-        student.GRADE = "A+"
-    }
-    else if (student.CGPA < 9.5 && student.CGPA >= 9) {
-        student.GRADE = "A"
-    }
+// const gradedDocument = doc.map((student) => {
+//     if(student.CGPA >= 9.5) {
+//         student.GRADE = "A+"
+//     }
+//     else if (student.CGPA < 9.5 && student.CGPA >= 9) {
+//         student.GRADE = "A"
+//     }
 
-    else if (student.CGPA < 9 && student.CGPA>= 8.5) {
-        student.GRADE = "B"
-    }
-    else if (student.CGPA < 8.5 && student.CGPA >= 8) {
-        student.GRADE = "C"
-    }
-    else if (student.CGPA < 8) {
-        student.GRADE = "D"
-    }    
-    return student;
-})
+//     else if (student.CGPA < 9 && student.CGPA>= 8.5) {
+//         student.GRADE = "B"
+//     }
+//     else if (student.CGPA < 8.5 && student.CGPA >= 8) {
+//         student.GRADE = "C"
+//     }
+//     else if (student.CGPA < 8) {
+//         student.GRADE = "D"
+//     }    
+//     return student;
+// })
 
-const filterDocument = gradedDocument.filter(student => student.CGPA > 8)
+// const filterDocument = gradedDocument.filter(student => student.CGPA > 8)
 
-gradedDocument.push({CGPA: averageCGPA, NAME: "Average Grades"})
-const excelDocument = json2xls(gradedDocument);
-fs.writeFileSync("Grades.xlsx", excelDocument, "binary")
+// gradedDocument.push({CGPA: averageCGPA, NAME: "Average Grades"})
+// const excelDocument = json2xls(gradedDocument);
+// fs.writeFileSync("Grades.xlsx", excelDocument, "binary")
 
-console.log(filterDocument);
+// console.log(filterDocument);
 
 
+const express = require("express")
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+server.listen(3000, ()=>console.log("Server started on port 300"));
